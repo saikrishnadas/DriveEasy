89c2a5fd9a9223b9e5cbfb12443e5d9c4a3225ad
Sai <sai@SAIs-MacBook-Pro.local>
2023-09-08 14:12:49 +0530

    adding map

---
+++ src/components/Map/MapBoxMap.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Map from 'react-map-gl';
+import "./MapBoxMap.css";
+
+function MapBoxMap() {
+    // let windowWidth = window.innerWidth * 0.52;
+    return (
+        <div className='mapbox-container'>
+            <Map
+                mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+                initialViewState={{
+                    longitude: -122.4,
+                    latitude: 37.8,
+                    zoom: 14
+                }}
+                style={{ width: 650, height: 400 }}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+            />
+        </div>
+    )
+}
+
+export default MapBoxMap
