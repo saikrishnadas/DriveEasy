d49fb00d6b159ce643adb710bdb8ce2de139662a
Sai <sai@SAIs-MacBook-Pro.local>
2023-09-24 17:55:56 +0530

    minor login fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -8,13 +9,14 @@ import store from "./store";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+	document.getElementById("root")
 );
 root.render(
 	<React.StrictMode>
 		<Auth0Provider
 			domain={String(process.env.REACT_APP_DOMAIN)}
 			clientId={String(process.env.REACT_APP_CLIENT_ID)}
+			redirectUri={window.location.origin}
 		>
 			<Provider store={store}>
 				<BrowserRouter>
